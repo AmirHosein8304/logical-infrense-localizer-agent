5f421eee07ab1b2b27d249e7dd457345ebd333ed
AmirHosein <ahtavakkoli96@gmail.com>
2025-06-03 18:10:05 +0330

    some changes

--- localizer-agent.py
+++ localizer-agent.py
@@ -4,6 +4,17 @@ from sympy import symbols
 from sympy.logic.boolalg import And, Or, Not, Implies
 from sympy.logic.inference import satisfiable
 
+def sens(maze,i,j):
+    sens = ''
+    for dr,dc in [[-1,0],[1,0],[0,-1],[0,1]]:
+        if i+dr >= 0 and i+dr < len(maze) and j+dc >= 0 and j+dc < len(maze[0]):
+            if maze[i+dr][j+dc] == 1:
+                sens += '1'
+            else:
+                sens += '0'
+        else:
+            sens += '1'
+    return sens
 with open('maze-map.txt','r') as f:
     maze = [list(map(int,line.split())) for line in f]
 first_pos = [rnd.randint(0,len(maze)-1),rnd.randint(0,len(maze[0])-1)]
@@ -15,7 +26,7 @@ pg.init()
 disp = pg.display.set_mode((len(maze[0])*80, len(maze)*80))
 disp.fill((255,255,255))
 
-symbol_names = [[f"wall_n{i}",f"wall_s{i}",f"wall_w{i}",f"wall_e{i}"] for i in range(len(maze)*len(maze[0]))]
+symbol_names = [[f"wall_n{i}",f"wall_s{i}",f"wall_w{i}",f"wall_e{i}",f"in_s{i}"] for i in range(len(maze)*len(maze[0]))]
 symbol_names = [val for item in symbol_names for val in item]
 names_string = " ".join(symbol_names)
 d_sym = symbols(names_string)
@@ -25,21 +36,30 @@ for i in range(len(maze)):
         if maze[i][j] == 1:
             pg.draw.rect(disp, (120,120,120), (j*80, i*80, 80, 80))
             pg.draw.rect(disp, (0,0,255), (j*80, i*80, 80, 80),2)
+            d_sym[(i*len(maze[0])+j)*5+4].subs({d_sym[(i*len(maze[0])+j)*5+4]:False})
         else:
             pg.draw.rect(disp, (0,0,255), (j*80, i*80, 80, 80),2)
             for k in range(4):
                 dr, dc = [-1,1,0,0][k], [0,0,-1,1][k]
                 if i+dr >= 0 and i+dr < len(maze) and j+dc >= 0 and j+dc < len(maze[0]):
                     if maze[i+dr][j+dc] == 1:
-                        d_sym[(i*len(maze[0])+j)*4+k].subs({d_sym[(i*len(maze[0])+j)*4+k]:True})
+                        d_sym[(i*len(maze[0])+j)*5+k].subs({d_sym[(i*len(maze[0])+j)*5+k]:True})
 
+same_sens = {sens(maze, 0, 0):[[i,j]]}
+for i in range(len(maze)):
+    for j in range(len(maze[0])):
+        if maze[i][j] == 0:
+            if sens(maze, i, j) == list(same_sens.keys())[-1]:
+                same_sens[sens(maze, i, j)].append([i,j])
+            else:
+                same_sens[sens(maze, i, j)] = [[i,j]]
+    
 runnig = True
 check = False
 history = []
 steps = 0
 
 while runnig and steps<10:
-    steps+=1
     pg.draw.rect(disp, (0, 255, 0),(first_pos[0]*80+5,first_pos[1]*80+5,70,70))
     pg.display.update()
     for event in pg.event.get():
@@ -52,16 +72,18 @@ while runnig and steps<10:
                     else:
                         sens += '0'
                 else:
-                    sens += '2'
+                    sens += '1'
             check = True
             history.append(sens)
         if event.type == pg.QUIT:
             pg.quit()
             running = False
         if event.type == pg.KEYDOWN:
+            steps+=1
             pg.draw.rect(disp, (255, 255, 255), (first_pos[0]*80+2, first_pos[1]*80+2, 76, 76))
             if event.key == pg.K_h:
                 print(history)
+                steps-=1
             if event.key in [pg.K_UP,pg.K_w]:
                 check = False
                 if first_pos[1] > 0 and maze[first_pos[1]-1][first_pos[0]] == 0:
